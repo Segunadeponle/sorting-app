77ef87c953dc6c4527643b3fc818f284bfd30d97
Segun <segunadeponle@gmail.com>
2017-08-17 09:06:28 +0100

    initial Commit.

---
+++ SortingApp/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace SortingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(603, 291);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 309);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(603, 46);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "203, 152, 157, 171, 90, 235, 164, 117, 83, 230, 189, 234, 119, 244, 116, 105, 186" +
+    "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(541, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(460, 404);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Mean";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(379, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Mode";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(298, 404);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Median";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Quick Sort",
+            "Insertion Sort",
+            "Bubble Sort"});
+            this.comboBox1.Location = new System.Drawing.Point(100, 404);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 409);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Sorting Agorithm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 372);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Time Take to Sort: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(117, 372);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Sort Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 439);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SortingApp/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingApp
+{
+    public partial class Form1 : Form
+    {
+        int[] array;
+        Thread thread;
+        DateTime startTime;
+        DateTime endTime;
+        string algorithm;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Start()
+        {
+            startTime = DateTime.Now;
+            switch (algorithm)
+            {
+                case "Quick Sort":
+                    QuickSort(array);
+                    break;
+                case "Insertion Sort":
+                    InsertionSort(array);
+                    break;
+                case "Bubble Sort":
+                    BubbleSort(array);
+                    break;
+
+                default:
+                    break;
+            }
+
+
+            try
+            {
+                endTime = DateTime.Now;
+                Invoke(new ThreadStart(UpadateTotalTime));
+            }
+            catch (Exception)
+            {
+
+                Thread.CurrentThread.Abort();
+            }
+
+        }
+
+        private void UpadateTotalTime()
+        {
+            var totalTime = endTime - startTime;
+
+            label3.Text = string.Format("{0} seconds",totalTime.TotalSeconds);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+            if (array!=null)
+            {
+                var b = new SolidBrush(Color.Black);
+                List<int> s = new List<int>();
+
+
+                var r = new Rectangle[array.Length];
+
+                for (int i = 0; i < r.Length; i++)
+                {
+                    var rr = array[i];
+                    r[i] = new Rectangle(3 + (5 * i), 290 - rr, 2, rr);
+                }
+
+                e.Graphics.FillRectangles(b, r);
+            }
+            
+        }
+        void BubbleSort(int[] x)
+        {
+            for (int i = 0; i < x.Length-1; i++)
+            {
+                for (int j = 0; j < x.Length-1-i; j++)
+                {
+                    if (x[j]>x[j+1])
+                    {
+                        Swap(x, j, j+1);
+
+                    }
+                }
+            }
+        }
+        public void InsertionSort(int[] a)
+        {
+            int N = a.Length;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (a[j] < a[j - 1])
+                    {
+                        Swap(a, j, j - 1);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        private int Partition(int[] a, int lo, int hi)
+        {
+            int i = lo, j = hi + 1;
+            while (true)
+            {
+                while (a[++i] < a[lo])
+                {
+                    if (i == hi) break;
+                }
+                while (a[lo] < a[--j])
+                {
+                    if (j == lo) break;
+                }
+                if (i >= j)
+                {
+                    break;
+                }
+                Swap(a, i, j);
+            }
+            Swap(a, lo, j);
+            return j;
+        }
+        private void QuickSort(int[] a, int lo, int hi)
+        {
+            if (hi <= lo) return;
+            int j = Partition(a, lo, hi);
+            QuickSort(a, lo, j - 1);
+            QuickSort(a, j + 1, hi);
+        }
+        private void QuickSort(int[] a)
+        {
+            QuickSort(a, 0, a.Length - 1);
+        }
+        private void Swap(int[] x, int i, int j)
+        {
+            var t = x[j];
+            x[j] = x[i];
+            x[i] = t;
+            Thread.Sleep(100);
+
+            try
+            {
+                Invoke(new ThreadStart(UpdatePanel));
+            }
+            catch (Exception)
+            {
+
+                Thread.CurrentThread.Abort();
+            }
+        }
+
+        private void UpdatePanel()
+        {
+            panel1.Refresh();
+        }
+        int [] GetNumbers()
+        {
+             var numbers = richTextBox1.Text.Split(new[] { '\r', '\n', ' ', ',' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            var t = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                t[i] = int.Parse(numbers[i]);
+            }
+            return t;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "";
+            algorithm = comboBox1.SelectedItem.ToString();
+            Console.WriteLine(algorithm);
+            
+
+            var numbers = GetNumbers();
+            if (numbers.Length!=0)
+            {
+                panel1.Refresh();
+                array = numbers;
+
+                if (thread != null)
+                {
+                    thread.Abort();
+                }
+                thread = new Thread(new ThreadStart(Start));
+                thread.Start();
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var nums =  GetNumbers();
+            double sum = 0;
+            foreach (var item in nums)
+            {
+                sum += item;
+            }
+            MessageBox.Show(string.Format("The mean of the distribution is {0}.",sum/nums.Length));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var nums = GetNumbers();
+            var mode = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (mode.ContainsKey(item))
+                {
+                    mode[item]++;
+                }
+                else
+                {
+                    mode.Add(item, 1);
+                }
+            }
+            int modeNumber = 0,max = -1;
+            foreach (var item in mode.Keys)
+            {
+                if (mode[item]>max)
+                {
+                    max = mode[item];
+                    modeNumber = item;
+                }
+            }
+            MessageBox.Show(string.Format("The mode of the distribution is {0} and it occured {1} times.", modeNumber,max));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double median;
+            var nums = GetNumbers();
+            
+            if (nums.Length%2==0)
+            {
+                median = (nums[(nums.Length) / 2-1]  + nums[(nums.Length) / 2])/2.0;
+            }
+            else
+            {
+                median = nums[nums.Length / 2];
+            }
+            
+
+            MessageBox.Show(string.Format("The median of the distribution is {0}.",median ));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label3.Text = "";
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
